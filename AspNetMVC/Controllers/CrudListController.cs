218bfcd68c8a10a707c38f2b4ae7906df364aa11
Adel <adel_alfa@hotmail.com>
2019-01-09 14:25:21 +0100

    Add project files.

---
+++ AspNetMVC/Controllers/CrudListController.cs
@@ -0,0 +1,116 @@
+﻿using AspNetMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMVC.Controllers
+{
+    public class CrudListController : Controller
+    {
+        
+        private List<Student> students = StudentModel.GetStudent();
+
+        // GET: CrudList
+        public ActionResult Index()
+        {
+            return View(students);
+        }
+        public ActionResult StudentDetails(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student st = students.Find(s => s.StudentId == id);
+            if (st == null)
+            {
+                return HttpNotFound();
+            }
+            return View(st);
+        }
+
+
+
+        // GET: Student/ UpdateStudentDetails/5
+        public ActionResult UpdateStudent(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = students.Find(s => s.StudentId == id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Students/ UpdateStudentDetails/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult UpdateStudent([Bind(Include = "StudentId,FullName,Email,ConfirmEmail,IsEnrol")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                var st = students.FirstOrDefault(s => s.StudentId == student.StudentId);
+                if (st != null) { st.FullName = student.FullName; st.Email = student.Email; st.IsEnrol = student.IsEnrol; }
+                return RedirectToAction("Index");
+
+            }
+            return View(student);
+        }
+        //============= Create New Student =================
+        public ActionResult AddStudent()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddStudent([Bind(Include = "StudentId,FullName,Email, ConfirmEmail,IsEnrol")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                if (students.Count < 50) students.Add(student);
+                return RedirectToAction("Index");
+            }
+
+            return View(student);
+        }
+
+        // GET: Student/DeleteStudent/ 
+        public ActionResult DeleteStudent(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = students.Find(s => s.StudentId == id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Student/DeleteStudent/5
+        [HttpPost, ActionName("DeleteStudent")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteStudentConfirmed(int id)
+        {
+            Student student = students.Find(s => s.StudentId == id);
+            if (student != null)
+                students.Remove(student);
+
+             //or we can use the next  if there is more than one match: 
+             //  students.RemoveAll(s => s.StudenId == id) 
+
+            return RedirectToAction("Index");
+        }       
+
+    }
+}
---
+++ AspNetMVC/Models/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AspNetMVC.Models
+{
+    public class StudentModel
+    {
+        private static List<Student> students = new List<Student>
+            {
+                new Student{StudentId=1, FullName="Ali Omar", Email="ali_omar@yahoo.com", IsEnrol=true},
+                 new Student{StudentId=2, FullName="Sarah Othman", Email="sarah.othman@gmail.com", IsEnrol=true},
+                  new Student{StudentId=3, FullName="Saad Suliman", Email="saad.suliman2020@gmail.com", IsEnrol=true}
+            };
+        public static List<Student> GetStudent()
+        {
+
+            return students;
+        }
+    }
+    public class Student
+    {
+        [Display(Name = "Student Id:")]
+        public int StudentId { get; set; }
+
+        [Display(Name = "Full Name:")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Full name is required")]
+        public string FullName { get; set; }      
+
+
+        [Display(Name = "Email Address:")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email address is required")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+
+        [Display(Name = "Confirm Email")]
+        [Compare("Email", ErrorMessage = "The Email and Confirm Email fields do not match.")]
+        public string ConfirmEmail { get; set; }
+
+        [Display(Name = "Is enrolled:")]
+        public bool IsEnrol { get; set; }
+    }
+}
